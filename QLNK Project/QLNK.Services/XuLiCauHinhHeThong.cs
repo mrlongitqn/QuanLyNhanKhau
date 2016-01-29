d67cbe3ed6c872207010e617720b0b894007c5fd
Nguyễn Ngọc Huy <romeohuy@gmail.com>
2016-01-29 19:03:05 +0700

    Service add class

---
+++ QLNK Project/QLNK.Services/XuLiCauHinhHeThong.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QLNK.EF;
+using QLNK.Repositories.Implements;
+using QLNK.Repositories.Interfaces;
+
+namespace QLNK.Services
+{
+   public class XuLiCauHinhHeThong : CauHinhChungRepository
+   {
+       private readonly CauHinhChungRepository _cauHinhChungRepository;
+       public XuLiCauHinhHeThong(IUnitOfWork unitOfWork, CauHinhChungRepository cauHinhChungRepository) : base(unitOfWork)
+       {
+           _cauHinhChungRepository = cauHinhChungRepository;
+       }
+
+       //public CauHinhChung GetByKey(string key)
+       //{
+       //     return _cauHinhChungRepository.
+       // }
+   }
+}
